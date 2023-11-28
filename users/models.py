1b38065dbf17e208017f0106e05fbf53afd6353d
chris <qkymzh123@gmail.com>
2023-11-28 13:52:49 +0900

    [Feat](users): Add Model Fields

--- users/models.py
+++ users/models.py
@@ -3,7 +3,17 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+        Other = ("other", "Other")
+
     name = models.CharField(
         max_length=150,
         default="",
+    )
+    avatar = models.URLField(blank=True)
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
     )
