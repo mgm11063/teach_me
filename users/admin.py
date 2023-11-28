04dc6825ac194a85cff6ede7c25ee4e07fdf8299
chris <qkymzh123@gmail.com>
2023-11-28 13:43:57 +0900

    [Setup](All): Setup The Project

---
+++ users/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": ("name",),
+                "classes": ("wide",),
+            },
+        ),
+    )
