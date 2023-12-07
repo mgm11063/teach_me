32784204974308cc7d9f7b0d4cd0b246d9eaecd0
chris <qkymzh123@gmail.com>
2023-12-07 15:45:23 +0900

    [Feat](users/profiles): Add Field and Reinforce

--- profiles/models.py
+++ profiles/models.py
@@ -9,18 +9,14 @@ class Profile(CoreModel):
         FEMALE = ("female", "Female")
         Other = ("other", "Other")
 
-    name = models.CharField(
-        max_length=150,
-        default="",
-    )
     user = models.ForeignKey(User, related_name="profiles", on_delete=models.CASCADE)
     avatar = models.URLField(blank=True)
     gender = models.CharField(
         max_length=10,
         choices=GenderChoices.choices,
     )
-    bio = models.TextField(max_length=500)
-    reward_points = models.IntegerField(default=0)
+    bio = models.TextField(max_length=500, blank=True)
+    reward_points = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
--- users/models.py
+++ users/models.py
@@ -1,5 +1,17 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(
+        max_length=15,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r"^01[016789]-\d{3,4}-\d{4}$",
+                message="휴대폰 번호 형식이 올바르지 않습니다. (예: 010-1234-5678)",
+            ),
+        ],
+        help_text="휴대폰 번호를 입력하세요. (예: 010-1234-5678)",
+    )
