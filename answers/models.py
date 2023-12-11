e05861c9e400e84cfae47e9312f12ec1c4294b26
chris <qkymzh123@gmail.com>
2023-12-11 11:57:52 +0900

    [Feat](answers): Add 'is_accepted' Field

--- answers/models.py
+++ answers/models.py
@@ -8,6 +8,7 @@ class Answer(CoreModel):
     content = models.TextField(max_length=999)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_accepted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.question.title
