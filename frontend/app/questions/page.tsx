b7860c547dfa86b1429c3f3d022c8a7bf3cd529d
chris <qkymzh123@gmail.com>
2023-12-14 15:04:34 +0900

    [F-Rename](home/questions): Page Title Rename

--- frontend/app/questions/page.tsx
+++ frontend/app/questions/page.tsx
@@ -1,8 +1,13 @@
+import type { Metadata } from 'next'
 import QuestionCards from "@/components/QuestionCards";
 
+export const metadata: Metadata = {
+    title: 'Questions',
+    description: 'Questions',
+}
+
+
 export default function Questions() {
-
-
     const projects = [
         {
             id: 1,
@@ -14,7 +19,6 @@ export default function Questions() {
                 { id: 1, name: '케인', avatar: 'https://static-cdn.jtvnw.net/jtv_user_pictures/66f0c1c8-e9d3-40ba-a626-e359dc4acb51-profile_image-300x300.png' },
             ],
         },
-
     ];
     return (
         <>
